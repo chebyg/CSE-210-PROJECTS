17a27ee8f4092dd535f11e63da00d461065a6907
chebyg <167763906+chebyg@users.noreply.github.com>
2024-10-05 21:23:58 -0700

    Update Program.cs
    Made it functional to ask the users name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+         Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstName}, {lastName}.");
+
+        Console.Write("What is your first name? ");
+        string firstFriendName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastFriendName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstFriendName}, {lastFriendName}.");
     }
 }
